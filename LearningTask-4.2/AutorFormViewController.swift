5ca2e904c3c478ae66ff181f51d49ebdf7138444
jeovanebarbosazup <jeovane.barbosa@zup.com.br>
2022-10-27 15:24:40 -0300

    Finished task 4.2

--- LearningTask-4.2/AutorFormViewController.swift
+++ LearningTask-4.2/AutorFormViewController.swift
@@ -9,9 +9,52 @@ import UIKit
 
 class AutorFormViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+    @IBOutlet weak var fotoTextField: UITextField!
+    @IBOutlet weak var bioTextField: UITextField!
+    @IBOutlet weak var autorTextField: UITextField!
+    @IBOutlet weak var tecnologiaTextField: UITextField!
+    
+    typealias ValidationMessage = String
+    
+    
+    @IBAction func buttonSalvarPressed(_ sender: UIButton) {
+        let (isFormValid, validationMessage) = checkForm()
+        
+        showAlert(checkIf:isFormValid, withMessage: validationMessage)
+        
+    }
+    
+    func checkForm() -> (Bool, ValidationMessage){
+        guard let foto = fotoTextField.text, !foto.isEmpty else{
+            return (false, "Informe a URL da foto do autor")
+        }
+        guard let nome = autorTextField.text, !nome.isEmpty else{
+            return (false, "Nome não pode estar em branco")
+        }
+        guard let bio = bioTextField.text, !bio.isEmpty else{
+            return (false, "A bio do autor não pode estar em branco")
+        }
+        guard let tecnologia = tecnologiaTextField.text, !tecnologia.isEmpty else{
+            return (false, "Informe as tecnologias sobre as quais o autor escreve")
+        }
+
+        return (true, "Autor cadastrado com Sucesso" )
+    }
+    
+    
+    func showAlert(checkIf formIsValid: Bool, withMessage validationMessage: ValidationMessage){
+        
+        if formIsValid {
+            let alert = UIAlertController(title: "Feito", message: validationMessage, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default))
+            self.present(alert, animated: true)
+        }else{
+            let alert = UIAlertController(title: "Erro", message: validationMessage, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default))
+            self.present(alert, animated: true)
+        }
+        
+        
+    }
 }
